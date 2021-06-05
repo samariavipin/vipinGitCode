a886d4ee77ed71618dde7f445c7788bcd9437b9a
VipinSamaria <vipin17208@iiitd.ac.in>
2021-06-05 18:24:01 +0530

    multiply file added

---
+++ multiply.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum{
+    public static void public static void main(String[] args) {
+        Scanner sc = new Scanner(system.in);
+        int a = 5;
+        int b = 10;
+
+        String s = sc.next();
+
+        if(s == "mul"){
+            System.out.println(a*b);
+        }
+    }
+}
