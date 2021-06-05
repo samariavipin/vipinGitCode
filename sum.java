60c26a634f75e115bec0ce315278062795bafd59
VipinSamaria <vipin17208@iiitd.ac.in>
2021-06-05 13:24:21 +0530

    added mesaage to sum file

--- sum.java
+++ sum.java
@@ -1,8 +1,14 @@
+import java.util.*;
 public class sum{
     public static void public static void main(String[] args) {
+        Scanner sc = new Scanner(system.in);
         int a = 5;
         int b = 10;
 
-        System.out.println(a+b);
+        String s = sc.next();
+
+        if(s == "add"){
+            System.out.println(a+b);
+        }
     }
 }
